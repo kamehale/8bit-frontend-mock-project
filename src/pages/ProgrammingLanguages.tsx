6eb5a6de6f637614cee0e5513e7237d1b5309720
Alessandro Cavalleri <alessandro.augusto.cavalleri@upc.edu>
2024-04-24 22:39:10 +0200

    Programming-languages-html

---
+++ src/pages/ProgrammingLanguages.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"; 
+const ProgrammingLanguages = () => { 
+return ( 
+<> 
+<div className="container text-left my-5" style={{paddingTop:100}}>
+This is the programming language page 
+</div> 
+</> 
+); 
+}; 
+export default ProgrammingLanguages;
