c682e4c636c746228967fb9b002acbcafced7231
Alessandro Cavalleri <alessandro.augusto.cavalleri@upc.edu>
2024-04-24 22:49:35 +0200

    Navbar

--- src/pages/ProgrammingLanguages.tsx
+++ src/pages/ProgrammingLanguages.tsx
@@ -1,7 +1,9 @@
 import * as React from "react"; 
+import Navbar from "../components/Navbar"
 const ProgrammingLanguages = () => { 
 return ( 
 <> 
+<Navbar/>
 <div className="container text-left my-5" style={{paddingTop:100}}>
 This is the programming language page 
 </div> 
